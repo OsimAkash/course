7c781f46cde69a3306fa7b60a96bc1ef2d830ee9
OsimAkash <osimakash123@gmail.com>
2022-12-23 23:45:10 +0600

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\http\Controllers;
+
+
+class HomeController extends Controller {
+
+
+    public function index() {
+        return "I am from controller";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
